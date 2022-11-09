498add342378b35f2dd0a0f46e9bb05a0f7f8b12
Maurice Roach <mroach1993@hotmail.com>
2022-11-09 13:25:55 -0500

    added scroll feature, social media links, changed font, modal popup

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const newBtn = document.querySelector(".new");
+const loginBtn = document.querySelector(".login_button");
+const container = document.querySelector(".container");
+const content = document.querySelector(".content");
+const closeBtn = document.querySelector(".close");
+const userID = document.querySelector(".userID");
 
-newBtn.addEventListener("click", function () {});
+const userLogin = document.querySelector(".userLogin");
+
+loginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  container.classList.add("show");
+  content.classList.add("show");
+  console.log("Username", userLogin.value);
+  userID.textContent = userLogin.value.toUpperCase();
+});
+
+closeBtn.addEventListener("click", function () {
+  container.classList.remove("show");
+  content.classList.remove("show");
+});
